248b3de28ea2aff2f628105fb3ee7555ae1b9d1a
Marvel-Ib <awesomewonder553@gmail.com>
2020-05-19 18:07:12 +0100

    done

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = 5000 || process.env.PORT;
+const expressLayouts = require("express-ejs-layouts");
 
 app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("Login");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("Contact");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 app.listen(PORT, () => {
   console.log("listening");
